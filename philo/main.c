70e6166d013d72f04499a315baa4b6c89b832ae0
scambier <scambier@student.42.fr>
2024-02-06 16:17:10 +0100

    Initial commit

---
+++ philo/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 16:07:41 by scambier          #+#    #+#             */
+/*   Updated: 2024/02/06 16:16:20 by scambier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <threads.h>
+
+// ARGS : number_of_philosophers, time_to_die, time_to_eat, time_to_sleep, [number_of_times_each_philosopher_must_eat]
+// EAT -> SLEEP -> THINK
+
+int	main(int argc, char **argv)
+{
+	
+}
