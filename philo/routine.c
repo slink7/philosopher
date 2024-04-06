757a8ada12d3c451e36fb6b68f7031dda8a21868
Sebastien Cambier <slink794@gmail.com>
2024-04-06 03:24:11 +0200

    Replaced mutexed printf by ft_sprintf

--- philo/routine.c
+++ philo/routine.c
@@ -6,23 +6,26 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 01:59:41 by scambier          #+#    #+#             */
-/*   Updated: 2024/04/06 02:05:19 by scambier         ###   ########.fr       */
+/*   Updated: 2024/04/06 03:22:20 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
+#include "libft.h"
 #include "header.h"
 
 void	*routine(void *arg)
 {
 	t_philosopher	*philo;
+	char			*temp;
 
 	philo = (t_philosopher *)arg;
-	pthread_mutex_lock(philo->table->pubmut + PRINTF);
-	printf("ABC %d .\n", philo->index);
-	pthread_mutex_unlock(philo->table->pubmut + PRINTF);
+	temp = ft_sprintf("ABC [%d] \t.\n", philo->index);
+	write(1, temp, ft_strlen(temp) + 1);
+	free(temp);
 	sleep(1);
 	return (0);
 }
