9880c49d12c1f3afe97c83c7b90df6f552a87920
Sebastien Cambier <slink794@gmail.com>
2024-04-07 03:06:04 +0200

    Added base routine

--- philo/routine.c
+++ philo/routine.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 01:59:41 by scambier          #+#    #+#             */
-/*   Updated: 2024/04/07 02:40:57 by scambier         ###   ########.fr       */
+/*   Updated: 2024/04/07 03:04:57 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,32 @@
 void	*routine(void *arg)
 {
 	t_philosopher	*philo;
+	t_mutex			*forks_a;
+	t_mutex			*forks_b;
 
 	philo = (t_philosopher *)arg;
-	ft_printf("ABC [%d] \t.\n", philo->index);
-	sleep(1);
+	if (philo->index == 0)
+	{
+		forks_a = &philo->table->forks[(philo->index + 1) % philo->params_cpy[SIZE]];
+		forks_b = &philo->table->forks[philo->index];
+	}
+	else
+	{
+		forks_a = &philo->table->forks[philo->index];
+		forks_b = &philo->table->forks[(philo->index + 1) % philo->params_cpy[SIZE]];
+	}
+	while (1)
+	{
+		pthread_mutex_lock(forks_a);
+		ft_printf("[%x]%d has taken a fork\n", (forks_a - philo->table->forks), philo->index);
+		pthread_mutex_lock(forks_b);
+		ft_printf("[%x]%d is eating\n", (forks_b - philo->table->forks), philo->index);
+		usleep(philo->params_cpy[TT_EAT] * 1000);
+		pthread_mutex_unlock(forks_b);
+		pthread_mutex_unlock(forks_a);
+		ft_printf("%d is sleeping\n", philo->index);
+		usleep(philo->params_cpy[TT_SLEEP] * 1000);
+
+	}
 	return (0);
 }
