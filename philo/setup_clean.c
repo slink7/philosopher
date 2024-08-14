68c02065a7d9ccfc231fc3afacea1b438c4d5c94
scambier <scambier@student.42.fr>
2024-08-14 17:20:31 +0200

    fixed small leak

--- philo/setup_clean.c
+++ philo/setup_clean.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 11:54:48 by scambier          #+#    #+#             */
-/*   Updated: 2024/08/14 16:47:32 by scambier         ###   ########.fr       */
+/*   Updated: 2024/08/14 17:19:53 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,7 @@ int	clear_table(t_table *table)
 
 	k = -1;
 	while (++k < (int)table->params[COUNT])
-		if (pthread_mutex_destroy(table->forks + k))
-			return (0);
+		pthread_mutex_destroy(table->forks + k);
 	free(table->forks);
 	return (1);
 }
